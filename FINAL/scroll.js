c18a14bd61400a3449364b7d26c1fd61362df0d4
mieliz <michaelelizarraraz@gmail.com>
2021-11-30 15:06:56 -0800

    Javascript

--- FINAL/scroll.js
+++ FINAL/scroll.js
@@ -17,7 +17,32 @@
     
       
       });
-    
+    $(".nav-link").click(function () {
+      console.log("fire")
+      $(".navbar-collapse").collapse("hide");
+    })
     });
       }(jQuery));
-  
+      // show logo then hide on scroll 
+      (function ($) {
+        $(document).ready(function(){
+          
+        
+        $(".logo").show();
+        
+        // fade in .navbar
+        $(function () {
+          $(window).scroll(function () {
+                  // set distance user needs to scroll before we fadeIn navbar
+            if ($(this).scrollTop() > 100  && $(this).width() < 450) {
+              $('.logo').fadeOut();
+            } else {
+              $('.logo').fadeIn();
+            }
+          });
+      
+        
+        });
+      
+      });
+        }(jQuery));
